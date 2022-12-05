55fbae5734ff1a226361bd257b2fd4d0d578d5cd
KaterynaKucherenko <estorskaya@gmail.com>
2022-12-05 18:59:08 +0300

    fix

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
-    boolean x = from1UpTo1000 <=500;
+    boolean x = from1UpTo1000 <500;
     if (x){
     System.out.println ("Eagle");}
     else {
